e5b4cc6babec5173e7df563b03963a1ee9482daa
Chu WANG <wc1225827513@gmail.com>
2021-10-19 20:40:24 +0800

    Step 4 and Step 5

--- procedural_representation.py
+++ procedural_representation.py
@@ -6,9 +6,11 @@ PATH_BEGIN = [1, 4, 3, 6, 7, 8, 5, 2]
 PATH_1 = [3, 6, 7, 8, 5, 4, 1, 0]
 PATH_2 = [1, 4, 3, 6, 7, 8, 5, 2]
 PATH_3 = [3, 6, 7, 8, 5, 4]
+PATH_4 = [5, 8, 7, 6, 3]
+PATH_5 = [3, 0, 1, 4, 5, 2, 1, 0, 3]
 
 
-def move_one_step(puzzle, path):
+def rotate_one_step(puzzle, path):
     route = []
     for position in path:
         route.append(puzzle[position])
@@ -23,34 +25,72 @@ def move_one_step(puzzle, path):
     return puzzle
 
 
+def exchange(puzzle, target, path):
+    for i in range(len(path) - 1):
+        if puzzle[path[i]] == target and i < len(path) - 1:
+            tmp = puzzle[path[i + 1]]
+            puzzle[path[i + 1]] = target
+            puzzle[path[i]] = tmp
+        display_puzzle(puzzle)
+    return puzzle
+
+
 def step_0(puzzle, path_begin):
+    print("Step 0")
     if puzzle[0] == 0:
         tmp = puzzle[1]
         puzzle[1] = 0
         puzzle[0] = tmp
     if puzzle[2] == 1 or puzzle[2] == 0:
-        move_one_step(puzzle, path_begin)
+        rotate_one_step(puzzle, path_begin)
         if puzzle[2] == 1 or puzzle[2] == 0:
-            move_one_step(puzzle, path_begin)
+            rotate_one_step(puzzle, path_begin)
     return puzzle
 
 
 def step_1(puzzle, path_1):
+    print("Step 1")
     while puzzle[0] != 1:
-        puzzle = move_one_step(puzzle, path_1)
+        puzzle = rotate_one_step(puzzle, path_1)
     return puzzle
 
 
 def step_2(puzzle, path_2):
+    print("Step 2")
     while puzzle[1] != 2:
-        puzzle = move_one_step(puzzle, path_2)
+        puzzle = rotate_one_step(puzzle, path_2)
     return puzzle
 
 
 def step_3(puzzle, path_3):
+    print("Step 3")
+    if puzzle[2] == 0:
+        tmp = puzzle[5]
+        puzzle[5] = 0
+        puzzle[2] = tmp
     while puzzle[4] != 3:
-        puzzle = move_one_step(puzzle, path_3)
+        puzzle = rotate_one_step(puzzle, path_3)
     return puzzle
+
+
+def step_4(puzzle, path_4):
+    print("Step 4")
+    while puzzle[3] != 0:
+        puzzle = rotate_one_step(puzzle, path_4)
+    return puzzle
+
+
+def step_5(puzzle, path_5):
+    print("Step 5")
+    puzzle = exchange(puzzle, 0, path_5)
+    return puzzle
+
+
+# def step_6(puzzle, path_6):
+#     print("Step 6")
+#     while puzzle[3] != 0:
+#         puzzle = rotate_one_step(puzzle, path_6)
+#     return puzzle
 
 
 puzzle = generate_puzzle()
@@ -58,3 +98,5 @@ puzzle = step_0(puzzle, PATH_BEGIN)
 puzzle = step_1(puzzle, PATH_1)
 puzzle = step_2(puzzle, PATH_2)
 puzzle = step_3(puzzle, PATH_3)
+puzzle = step_4(puzzle, PATH_4)
+puzzle = step_5(puzzle, PATH_5)
